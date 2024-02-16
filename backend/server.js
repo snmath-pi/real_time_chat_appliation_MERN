0db58e210bba19fca1b10fcd09f93a49e4017329
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2024-02-16 17:51:11 +0530

    c

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,33 @@
 import express from 'express'
 import dotenv from 'dotenv'
-const app = express();
 import authRoutes from './routes/auth.routes.js'
+import messageRoutes from './routes/message.routes.js'
+import connecttoMongoDB from './db/connectToMongodb.js';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+
+const PORT = process.env.PORT || 5000;
 
 dotenv.config(); // to run this port at 8000
-const PORT = process.env.PORT || 5000;
+
 
 app.get("/", (req, res)=>{
     // root route http://localhost:5000/
     res.send('hello world!!')
 })
 
+// middleware
+app.use(express.json()); // to parse the incoming requests with JSON payloads(from req.body)
+app.use(cookieParser());
+
 
 app.use("/api/auth", authRoutes)
-app.listen(PORT, ()=> console.log(`Server is running ${PORT}`));
+app.use("/api/messages", messageRoutes)
+
+
+app.listen(PORT, ()=> {
+    connecttoMongoDB();
+    console.log(`Server is running on port ${PORT}`);
+});
